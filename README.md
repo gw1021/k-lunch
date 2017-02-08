667c52f3e4f279eeba357a223b9a3e93e8758d51
saxman6989 <ohmyshoulder987@gmail.com>
2017-02-08 15:45:31 +0900

    Added padding to date

--- README.md
+++ README.md
@@ -15,48 +15,32 @@ A node.js module for serving lunch-menu.
 
 #### This module is at Alpha-stage. There's no error handling and may not behave as you intended.
 
-## Example
-```js
-let express = require('express');
-let klunch = require('k-lunch');
-
-let router = express.Router();
-
-router.get('/', function(req, res, next) {
-  const year = req.query.yy,
-    month = req.query.mm,
-    day = req.query.dd,
-    time = req.query.time,
-    name = req.query.name,
-    code = req.query.code,
-    phase = req.query.phase;
-
-  const form = {
-    year: year,
-    month: month,
-    day: day,
-    time: time,
-    name: name,
-    code: code,
-    phase: phase
-  }
-
-  klunch.getLunch(form, (output) => {
-    res.json(output);
-  });
-});
-
-module.exports = router;
-```
-
 ## Installation
 ```
 npm install k-lunch
 ```
 
+## Example
+```js
+const klunch = require('k-lunch');
+
+const form = {
+  year: '2017',
+  month: '02',
+  day: '07',
+  time: 2, // Breakfast = 1, Lunch = 2, Dinner = 3
+  name: schoolName,
+  code: schoolCode,
+  phase: 4 // Elementary School = 2, Middle School = 3, High School = 4
+}
+
+klunch.getLunch(form, (err, output) => {
+  if(err) throw err;
+  console.log(output);
+}
+```
+
 ## Form
-*** You need to convert year, month, day to String manually! ***
-(Will implement feature about this in the future.)
 
 #### Examples
 ```js
--- index.js
+++ index.js
@@ -1,105 +1,107 @@
-let cheerio = require('cheerio');
-let request = require('request');
+const cheerio = require('cheerio');
+const request = require('request');
 
-function max_date(year, month){
-	switch(month){
-		case 1:
-		case 3:
-		case 5:
-		case 7:
-		case 8:
-		case 10:
-		case 12:
-			return 31;
-			break;
-		case 4:
-		case 6:
-		case 9:
-		case 11:
-			return 30;
-			break;
-		case 2:
-			if(year % 4 === 0){
-				return 29;
-			}
-			else{
-				return 28;
-			}
-			break;
-		default:
-			return 30;
-			break;
-	}
+const max_date = (year, month) => {
+  switch(month){
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      return 31;
+      break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      return 30;
+      break;
+    case 2:
+      if(year % 4 === 0){
+        return 29;
+      }
+      else{
+        return 28;
+      }
+      break;
+    default:
+      return 30;
+      break;
+  }
 }
 
-function pad(d) {
-	return (d < 10) ? '0' + d.toString() : d.toString();
-}
+const pad = d => (d < 10) ? '0' + d.toString() : d.toString();
 
 const URL = 'http://stu.goe.go.kr/sts_sci_md01_001.do';
 
 module.exports.getLunch = function(form, callback) {
-	const reqForm = {
-		"schYmd": form.year + form.month + form.day,
-		"schMmealScCode": form.time,
-		"insttNm": form.name,
-		"schulCode": form.code,
-		"schulKndScCode": pad(form.phase),
-		"schulCrseScCode": form.phase
-	}
+  const ymdArr = [form.year, form.month, form.day];
+  const ymd = ymdArr.map(v => pad(v)).join('');
+  const reqForm = {
+    "schYmd": ymd,
+    "schMmealScCode": form.time,
+    "insttNm": form.name,
+    "schulCode": form.code,
+    "schulKndScCode": pad(form.phase),
+    "schulCrseScCode": form.phase
+  }
 
-	request({
-		headers: {'content-type' : 'application/x-www-form-urlencoded'},
-		url: 'http://stu.goe.go.kr/sts_sci_md01_001.do',
-		form: reqForm
-	}, function(err, res_post, body){
-		if(err) {
-			callback(err);
-		} else {
-			let $ = cheerio.load(body, {decodeEntities: false});
+  request({
+    headers: {'content-type' : 'application/x-www-form-urlencoded'},
+    url: URL,
+    form: reqForm
+  }, function(err, res_post, body){
+    if(err) {
+      callback(err, null);
+    } else {
+      const $ = cheerio.load(body, {decodeEntities: false});
+      const mixedDate = ymdArr.map(v => pad(v)).join('.');
 
-			const mixedDate = [form.year, form.month, form.day].join('.');
+      let index = 0;
+      let data = '';
+      let dataArr = [];
+      let output = [];
 
-			let index = 0;
-			let data = '';
-			let dataArr = [];
-			let output = [];
+      $('table > thead > tr').each(function() {
+        let rows = $(this).find('th');
+        
+        for(let i = 1; i < 8; i++) {
+          let rowText = rows.eq(i).text();
+          rowText.search(mixedDate) !== -1 ? index = i : i
+        }
+      });
 
-			$('table > thead > tr').each(function() {
-				let rows = $(this).find('th');
+      $('table > tbody').each(function() {
+        let lunchTr = $(this).find('tr').eq(1);
+        let lunchInfo = lunchTr.find('td').eq(index - 1).html();
+        data = lunchInfo;
+      });
 
-				for(let i = 1; i < 8; i++) {
-					let rowText = rows.eq(i).text();
-					rowText.search(mixedDate) !== -1 ? index = i : i
-				}
-			});
+      if(data === null || $('table > thead > tr > td').text() === '자료가 없습니다.') {
+        callback("There's no data in table.", null);
+      } else {
+        dataArr = data.split('<br>');
+        dataArr.pop();
 
-			$('table > tbody').each(function() {
-				let lunchTr = $(this).find('tr').eq(1);
-				let lunchInfo = lunchTr.find('td').eq(index - 1).html();
-				data = lunchInfo;
-			});
+        dataArr.forEach(e => {
+          let intIndex = e.search(/\d/); //finds first int and returns index of it.
+          let dataScheme = {
+            menus: '',
+            nuts: ''
+          }
+          dataScheme.menus = e.substring(0, intIndex);
+          dataScheme.nuts = e.substring(intIndex, e.length - 1).split('.');
+          output.push(dataScheme);
+        });
 
-			dataArr = data.split('<br>');
-			dataArr.pop();
-
-			dataArr.forEach(e => {
-				let intIndex = e.search(/\d/); //finds first int and returns index of it.
-				let dataScheme = {
-					menus: '',
-					nuts: ''
-				}
-
-				dataScheme.menus = e.substring(0, intIndex);
-				dataScheme.nuts = e.substring(intIndex, e.length - 1).split('.');
-				output.push(dataScheme);
-			});
-
-			callback(output);
-		}
-	});
+        if(output[0] == null) {
+          callback("There's no data in table.", null);
+        } else {
+          callback(null, output);
+        }        
+      }
+    }
+  });
 }
-
-/*if($('table > thead > tr > td').text() === '자료가 없습니다.') {
-	res.json('Error!')
-}*/
